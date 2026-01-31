0bdeea323ac74a675783a13ad500b6dc66e6c40e
Gayathri Balla <ballagayatri18@gmail.com>
2026-01-31 19:30:54 +0530

    Add basic App component

---
+++ server/client/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Eatoes Admin Dashboard</h1>
+      <p>Welcome to the Restaurant Admin Panel</p>
+    </div>
+  );
+}
+
+export default App;
